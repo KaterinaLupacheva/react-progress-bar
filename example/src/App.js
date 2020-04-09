d1c36afd621a87d514994c0920ad435929431bd6
Katerina <katerinalupacheva@gmail.com>
2020-04-09 18:11:13 +0300

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import { ExampleComponent } from '@ramonak/react-progress-bar'
-import '@ramonak/react-progress-bar/dist/index.css'
+import ProgressBar from "@ramonak/react-progress-bar";
+import "@ramonak/react-progress-bar/dist/index.css";
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  const [completed, setCompleted] = useState(0);
 
-export default App
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
+  return (
+    <div className="App">
+      <ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React from "react";
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+const ProgressBar = props => {
+  const { bgcolor, completed } = props;
+
+  const containerStyles = {
+    height: 20,
+    width: "100%",
+    backgroundColor: "#e0e0de",
+    borderRadius: 50,
+    margin: 50
+  };
+
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    transition: "width 1s ease-in-out",
+    borderRadius: "inherit",
+    textAlign: "right"
+  };
+
+  const labelStyles = {
+    padding: 5,
+    color: "white",
+    fontWeight: "bold"
+  };
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
