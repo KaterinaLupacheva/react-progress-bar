10df42f7931743270733675db2f8084d32c83395
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-12-07 22:15:01 +0300

    Fetch API updated

--- example/src/useViewCounter.hook.js
+++ example/src/useViewCounter.hook.js
@@ -8,7 +8,7 @@ const useViewCounter = slug => {
       return;
     }
 
-    fetch(`https://cors-anywhere.herokuapp.com/http://localhost:3000/api/views/${slug}`)
+    fetch(`http://localhost:3000/api/views/${slug}`)
       .then(res => res.json())
       .then(json => {
         setViews(json.views);
