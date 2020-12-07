907bc15d73967d9ac00296b03687aa1be3496bd6
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-12-07 20:00:08 +0300

    Updates

--- precache-manifest.25b7a452255ddfdf330238d7976239da.js
+++ precache-manifest.25b7a452255ddfdf330238d7976239da.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "51fc181f3056d1422bbde72a6f90eed8",
+    "revision": "34fc96f46c48d2035129a0fc98c49e96",
     "url": "/react-progress-bar/index.html"
   },
   {
-    "revision": "296fceda66a4a97e4b60",
+    "revision": "3193a1279a46692321e5",
     "url": "/react-progress-bar/static/css/main.9a4845a1.chunk.css"
   },
   {
@@ -16,12 +16,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-progress-bar/static/js/2.3091ddba.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "296fceda66a4a97e4b60",
-    "url": "/react-progress-bar/static/js/main.3f333676.chunk.js"
+    "revision": "3193a1279a46692321e5",
+    "url": "/react-progress-bar/static/js/main.e931ca0b.chunk.js"
   },
   {
     "revision": "6fce53c7c7713ebf61712cc2929746fa",
-    "url": "/react-progress-bar/static/js/main.3f333676.chunk.js.LICENSE.txt"
+    "url": "/react-progress-bar/static/js/main.e931ca0b.chunk.js.LICENSE.txt"
   },
   {
     "revision": "5b74d6fb10bdde79bfde",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-progress-bar/precache-manifest.52dc113c55b5b13d5265bed3d9794078.js"
+  "/react-progress-bar/precache-manifest.25b7a452255ddfdf330238d7976239da.js"
 );
 
 self.addEventListener('message', (event) => {
