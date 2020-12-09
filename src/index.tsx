3cb12ffa3b38eb6d3fa25151213eab0ec29ec5be
KaterinaLupacheva <katerinalupacheva@gmail.com>
2020-12-09 11:57:56 +0300

    index.tsx added

---
+++ src/index.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+
+export type ProgressBarProps = {
+  completed: number;
+  bgcolor?: string;
+  baseBgColor?: string;
+  height?: string;
+  width?: string;
+  borderRadius?: string;
+  margin?: string;
+  padding?: string;
+  labelAlignment?: "left" | "center" | "right" | "outside";
+  labelColor?: string;
+  labelSize?: string;
+};
+
+const ProgressBar: React.FC<ProgressBarProps> = (props) => {
+  const {
+    bgcolor,
+    completed,
+    baseBgColor,
+    height,
+    width,
+    margin,
+    padding,
+    borderRadius,
+    labelAlignment,
+    labelColor,
+    labelSize,
+  } = props;
+
+  const getAlignment = (
+    alignmentOption: ProgressBarProps["labelAlignment"]
+  ) => {
+    if (alignmentOption === "left") {
+      return "flex-start";
+    } else if (alignmentOption === "center") {
+      return "center";
+    } else if (alignmentOption === "right") {
+      return "flex-end";
+    }
+  };
+
+  const alignment = getAlignment(labelAlignment);
+
+  const containerStyles: React.CSSProperties = {
+    height: height,
+    backgroundColor: baseBgColor,
+    borderRadius: borderRadius,
+    padding: padding,
+    width: width,
+    margin: margin,
+  };
+
+  const fillerStyles: React.CSSProperties = {
+    height: height,
+    width: completed > 100 ? `100%` : `${completed}%`,
+    backgroundColor: bgcolor,
+    transition: "width 1s ease-in-out",
+    borderRadius: "inherit",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: labelAlignment !== "outside" && alignment,
+  };
+
+  const labelStyles: React.CSSProperties = {
+    padding: labelAlignment === "outside" ? "0 0 0 5px" : "5px",
+    color: labelColor,
+    fontWeight: "bold",
+    fontSize: labelSize,
+  };
+
+  const outsideStyles = {
+    display: labelAlignment === "outside" && "flex",
+    alignItems: labelAlignment === "outside" && "center",
+  };
+
+  return (
+    <div style={outsideStyles}>
+      <div style={containerStyles}>
+        <div style={fillerStyles}>
+          {labelAlignment !== "outside" && (
+            <span style={labelStyles}>{`${completed}%`}</span>
+          )}
+        </div>
+      </div>
+      {labelAlignment === "outside" && (
+        <span style={labelStyles}>{`${completed}%`}</span>
+      )}
+    </div>
+  );
+};
+
+ProgressBar.defaultProps = {
+  bgcolor: "#6a1b9a",
+  height: "20px",
+  width: "100%",
+  borderRadius: "50px",
+  labelAlignment: "right",
+  baseBgColor: "#e0e0de",
+  labelColor: "#fff",
+  labelSize: "15px",
+};
+
+export default ProgressBar;
